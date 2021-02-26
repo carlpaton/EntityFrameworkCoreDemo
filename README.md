10b5f54e79aa4351f56a942a75dd62a86083dccc
Carl <carl.paton@gmail.com>
2021-02-26 23:18:09 +1300

    initial

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# EntityFrameworkCoreDemo
+### Entity Framework Core Demo
+
+* https://carlpaton.github.io/2021/02/entity-framework-core/
+
---
+++ src/SweetApp/Infrastructure/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SweetApp.Domain.Models;
+
+namespace SweetApp.Infrastructure
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer("Data Source=localhost,1401;Initial Catalog=SweetApp;Integrated Security=False;User Id=sa;Password=2dd73426-d37a-4fd2-89e9-92f2a8f1a526;Pooling=False");
+    }
+}
